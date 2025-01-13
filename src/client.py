ddb2fcd2742d505819f28da5755e89e53752e5b5
João Gabriel <jg.lofiego@gmail.com>
2025-01-13 18:16:23 -0300

    Adiciona reconexão por parte do client

--- src/client.py
+++ src/client.py
@@ -1,15 +1,30 @@
-import socket
-import threading
+import socket, threading, time
 
+
+def reconnect():
+    while True:
+        try:
+            print("Tentando reconectar...")
+            client = socket.create_connection(("localhost", 3000))
+            print("Conexão restabelecida!")
+            break
+        except:
+            time.sleep(5)
+            pass
+    return client
+
+SERVER_HOST = "localhost"
 HOST = "127.0.0.1"
 PORT = 3000
 
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect((HOST, PORT))
+try:
+    client = socket.create_connection((SERVER_HOST, PORT))
+    print("Conexão estabelecida!")
+except:
+    client = reconnect()
+    
 
-print("Conexão estabelecida!")
-
-def receive():
+def receive(client: socket.socket):
     while True:
         try:
             message = client.recv(1024).decode("ascii")
@@ -31,7 +46,7 @@ def receive():
 
             elif message == "REGISTER_SUCCESS":
                 print("Cadastro realizado com sucesso!")
-                thread_wrt = threading.Thread(target=write)
+                thread_wrt = threading.Thread(target=write, args=(client,))
                 thread_wrt.start()
 
             elif message == "USER_NOT_FOUND":
@@ -42,7 +57,7 @@ def receive():
 
             elif message == "LOGIN_SUCCESS":
                 print("Login realizado com sucesso!")
-                thread_wrt = threading.Thread(target=write)
+                thread_wrt = threading.Thread(target=write, args=(client,))
                 thread_wrt.start()
 
             else:
@@ -50,17 +65,18 @@ def receive():
         except:
             print("Um erro ocorreu!")
             client.close()
-            break
+            client = reconnect()
 
-def write():
+def write(client: socket.socket):
     while True:
         try:
-            message = input()
+            message = input("Digite sua mensagem: ")
             client.send(message.encode("ascii"))
-        except:
-            print("Erro ao enviar mensagem.")
-            client.close()
+        except EOFError:
+            print("Mensagem inválida.")
+        except OSError:
+            print("Não foi possível enviar mensagem.")
             break
 
-thread_rcv = threading.Thread(target=receive)
+thread_rcv = threading.Thread(target=receive, args=(client,))
 thread_rcv.start()
--- src/server.py
+++ src/server.py
@@ -2,7 +2,7 @@ import socket
 import threading
 import os
 
-HOST = "127.0.0.1"
+HOST = "localhost"
 PORT = 3000
 MAX_USERS = 5
 
@@ -51,40 +51,62 @@ def handle(client: socket.socket):
 
 def authenticate(client: socket.socket):
     while True:
+        addr = client.getpeername()
+        
         client.send("REGISTER_OR_LOGIN".encode("ascii"))
+        print(f"Requisitando escolha de autenticação de {addr}")
         choice = client.recv(1024).decode("ascii")
 
         if choice == "REGISTER":
+            print(f"Registro escolhido por {addr}")
+            
+            print(f"Requisitando username de {addr}")
             client.send("USERNAME".encode("ascii"))
+            
             username = client.recv(1024).decode("ascii")
+            print(f"Usuario recebido de {addr}: {username}")
 
             if username in users:
+                print(f"Usuário {username} já existe.")
                 client.send("USER_EXISTS".encode("ascii"))
                 continue
 
+            print(f"Requisitando senha de {addr}")
             client.send("PASSWORD".encode("ascii"))
             password = client.recv(1024).decode("ascii")
+            print(f"Senha recebida de {addr}")
 
             users[username] = password
             save_user(username, password)
+            
+            print(f"Usuário {username} registrado com sucesso em {addr}.")
             client.send("REGISTER_SUCCESS".encode("ascii"))
             return username
 
         elif choice == "LOGIN":
+            print(f"Login escolhido por {addr}")
+            
+            print(f"Requisitando username de {addr}")
             client.send("USERNAME".encode("ascii"))
             username = client.recv(1024).decode("ascii")
+            print(f"Usuario recebido de {addr}: {username}")
 
             if username not in users:
+                print(f"Usuário {username} não encontrado.")
                 client.send("USER_NOT_FOUND".encode("ascii"))
                 continue
 
+            print(f"Requisitando senha de {addr}")
             client.send("PASSWORD".encode("ascii"))
             password = client.recv(1024).decode("ascii")
+            print(f"Senha recebida de {addr}")
 
             if users[username] != password:
+                print(f"Senha incorreta para o usuário {username}.")
                 client.send("INVALID_PASSWORD".encode("ascii"))
                 continue
 
+            print(f"Usuário {username} logado com sucesso em {addr}.")
             client.send("LOGIN_SUCCESS".encode("ascii"))
             return username
 
